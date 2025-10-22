3a3d80b0da102e1d6e7c04664eaf10020aeb54dc
huzaifa wahed shaikh <patilhuzaifa@gmail.com>
2025-10-22 13:57:43 +0530

    completed all modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+MONGO_DATABASE_NAME = 'pwskills'
+MONGO_COLLECTION_NAME = 'wafer_fault'
+
+
+
+
+
+
+MONGO_DB_URL = 'mongodb+srv://huzaifa_shaikh:SSbmHtmENl9CBMNw@cluster0.oilbggp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+MODEL_FILE_NAME = 'model'
+MODEL_FILE_EXTENSION = '.pkl'
+
+artifact_folder = 'artifacts'
